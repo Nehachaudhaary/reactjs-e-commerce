d6c1dd0503c4891f6fda34432338cda9475b56ed
Nehachaudhaary <nehachaudharync04@gmail.com>
2022-05-03 17:48:27 +0530

    initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className='page section section-center'>
+        <img src={aboutImg} alt='nice desk' />
+        <article>
+          <div className='title'>
+            <h2>our story</h2>
+            <div className='underline'></div>
+          </div>
+          <p>
+          Los Angeles-based brand COVERSTORY aims to redefine the classics through a modern lens. The denim pieces are made with careful attention to detail, 
+        which means sourcing premium materials from around the world and personally fit-testing each design to ensure the cut is just right.
+        We believe the shift towards circularity needn’t be complicated. Therefore we have designed a flexible step-by-step onboarding process that enables fashion brands to join the circular economy.
+         Our brand partners benefit from an additional source of revenue, increased sustainability credentials and the time to grow their brand, safe in the knowledge that our processes are agile, ethical, sustainable.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  )
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
